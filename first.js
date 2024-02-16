5a833f9d5e43771d72e5d7ec22a52abb4054bc6b
Jashrajgor <98871087+Jashrajgor@users.noreply.github.com>
2024-02-16 19:44:15 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,22 @@
+// importing the file module that is in-built module of node.js which provide some functionality of reading a file content and writing into the file;
+const fs=require("fs");
+fs.readFile('something.txt','utf-8',(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+    }
+})
+
+// To Write the any text file with help of the node there is to method 1-> fs.weriteFile("file_name","content_to_written int the file",()=>{})=>it accepts three arguments and another is fs.createWriteStream("file_name").
+
+fs.writeFile("created_file.txt","file is create with the help of writeFile mehtod of fs object",(err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("File created successfully");
+    }
+})
+
